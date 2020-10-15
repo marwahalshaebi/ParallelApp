80d1d5658b896292c48dc26cd62d00b31809e15a
Marwah Alshaebi <marwahalshaebi@gmail.com>
2020-10-15 13:35:29 -0400

    i wrote a comment

--- app/src/main/java/com/example/parallel/MainActivity.java
+++ app/src/main/java/com/example/parallel/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+ // this is the welcome page
     public void logout(View view) {
         FirebaseAuth.getInstance().signOut();//logout
         startActivity(new Intent(getApplicationContext(),Login.class));
