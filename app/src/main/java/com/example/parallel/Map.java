19226211d5bcd359788302ed9004936bbcf28b8a
Owner <hersi.abduljabbar@gmail.com>
2020-11-02 18:42:36 -0500

    Map Access Location

--- app/src/main/java/com/example/parallel/Map.java
+++ app/src/main/java/com/example/parallel/Map.java
@@ -12,27 +12,293 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+//public class Map extends FragmentActivity implements OnMapReadyCallback {
+//    GoogleMap mapAPI;
+//    SupportMapFragment mapFragment;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_maps);
+//
+//        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapAPI);
+//
+//        mapFragment.getMapAsync(this);
+//    }
+//
+//    // sets map location to ottawa, must change to implement location services
+//    @Override
+//    public void onMapReady(GoogleMap googleMap){
+//        mapAPI = googleMap;
+//
+//        LatLng Ottawa = new LatLng(45.411073, -75.696295);
+//        mapAPI.addMarker(new MarkerOptions().position(Ottawa).title("Ottawa"));
+//        mapAPI.moveCamera(CameraUpdateFactory.newLatLng(Ottawa));
+//    }
+//}
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
 public class Map extends FragmentActivity implements OnMapReadyCallback {
-    GoogleMap mapAPI;
-    SupportMapFragment mapFragment;
+
+    private GoogleMap mMap;
+    LocationManager locationManager;
+
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+    Marker marker;
+    LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-
-        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapAPI);
-
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapAPI);
         mapFragment.getMapAsync(this);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        //if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION);
+            //return;
+        }else{
+
+            locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(@NonNull Location location) {
+                    //zoomLevel to magnify user location on map
+                    float zoomLevel = 15f;
+
+                    //get the latitude
+                    double latitude = location.getLatitude();
+
+                    //get the longitude
+                    double longitude = location.getLongitude();
+
+                    //Instantiate the class Geocoder, converting lat long to meaningful addresses locations
+                    Geocoder geocoder = new Geocoder(getApplicationContext());
+                    try {
+                        List<Address> addressList = geocoder.getFromLocation(latitude, longitude, 1);
+                        String strResult = addressList.get(0).getLocality() + " : ";
+                        strResult += addressList.get(0).getCountryName();
+
+                        LatLng latLng = new LatLng(latitude, longitude);
+                        if(marker != null){
+                            marker.remove();
+                            marker = mMap.addMarker(new MarkerOptions().position(latLng).title(strResult));
+                            mMap.setMaxZoomPreference(20);
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoomLevel));
+                        }else{
+                            marker = mMap.addMarker(new MarkerOptions().position(latLng).title(strResult));
+                            mMap.setMaxZoomPreference(20);
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoomLevel));
+                        }
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) { }
+
+                @Override
+                public void onProviderEnabled(String provider) { }
+
+                @Override
+                public void onProviderDisabled(String provider) {}
+
+            };
+
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+
+
+/*
+        //checking if the network provider is enabled
+        if(locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(@NonNull Location location) {
+                    //zoomLevel to magnify user location on map
+                    float zoomLevel = 15f;
+
+                    //get the latitude
+                    double latitude = location.getLatitude();
+
+                    //get the longitude
+                    double longitude = location.getLongitude();
+
+                    //Instantiate the class LatLng
+                    LatLng latLng = new LatLng(latitude, longitude);
+
+                    //Instantiate the class Geocoder, converting lat long to meaningful addresses
+                    Geocoder geocoder = new Geocoder(getApplicationContext());
+                    try {
+                        List<Address> addressList = geocoder.getFromLocation(latitude, longitude, 1);
+                        String str = addressList.get(0).getLocality() + ",";
+                        str += addressList.get(0).getCountryName();
+
+                        mMap.addMarker(new MarkerOptions().position(latLng).title(str));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoomLevel));
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+
+ */
+        /*
+        else if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(@NonNull Location location) {
+                    //zoomLevel to magnify user location on map
+                    float zoomLevel = 15f;
+
+                    //get the latitude
+                    double latitude = location.getLatitude();
+
+                    //get the longitude
+                    double longitude = location.getLongitude();
+
+                    //Instantiate the class LatLng
+                    LatLng latLng = new LatLng(latitude, longitude);
+
+                    //Instantiate the class Geocoder, converting lat long to meaningful addresses
+                    Geocoder geocoder = new Geocoder(getApplicationContext());
+                    try {
+                        List<Address> addressList = geocoder.getFromLocation(latitude, longitude, 1);
+                        String str = addressList.get(0).getLocality() + ",";
+                        str += addressList.get(0).getCountryName();
+
+                        mMap.addMarker(new MarkerOptions().position(latLng).title(str));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoomLevel));
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+
+         */
     }
 
-    // sets map location to ottawa, must change to implement location services 
     @Override
-    public void onMapReady(GoogleMap googleMap){
-        mapAPI = googleMap;
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(@NonNull Location location) {
+                    //zoomLevel to magnify user location on map
+                    float zoomLevel = 15f;
 
-        LatLng Ottawa = new LatLng(45.411073, -75.696295);
-        mapAPI.addMarker(new MarkerOptions().position(Ottawa).title("Ottawa"));
-        mapAPI.moveCamera(CameraUpdateFactory.newLatLng(Ottawa));
+                    //get the latitude
+                    double latitude = location.getLatitude();
+
+                    //get the longitude
+                    double longitude = location.getLongitude();
+
+                    //Instantiate the class Geocoder, converting lat long to meaningful addresses locations
+                    Geocoder geocoder = new Geocoder(getApplicationContext());
+                    try {
+                        List<Address> addressList = geocoder.getFromLocation(latitude, longitude, 1);
+                        String strResult = addressList.get(0).getLocality() + " : ";
+                        strResult += addressList.get(0).getCountryName();
+
+                        LatLng latLng = new LatLng(latitude, longitude);
+                        if(marker != null){
+                            marker.remove();
+                            marker = mMap.addMarker(new MarkerOptions().position(latLng).title(strResult));
+                            mMap.setMaxZoomPreference(20);
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoomLevel));
+                        }else{
+                            marker = mMap.addMarker(new MarkerOptions().position(latLng).title(strResult));
+                            mMap.setMaxZoomPreference(20);
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoomLevel));
+                        }
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) { }
+
+                @Override
+                public void onProviderEnabled(String provider) { }
+
+                @Override
+                public void onProviderDisabled(String provider) {}
+
+            };
+
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        //float zoomLevel = 15f; //10.2f;
+
+        // Add a marker in Sydney and move the camera
+        //LatLng sydney = new LatLng(-34, 151);
+/*        LatLng ottawa = new LatLng(45.4215, -75.6972);
+        mMap.addMarker(new MarkerOptions().position(ottawa).title("Marker in Ottawa"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ottawa, zoomLevel ));
+*/
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        locationManager.removeUpdates(locationListener);
     }
 }
