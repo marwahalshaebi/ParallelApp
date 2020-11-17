6830a65c55e8160e10def7731a941300505c86c2
t-emala <t-emala@microsoft.com>
2020-11-17 10:05:23 -0500

    working on retreiving fcm device toke

--- app/src/main/java/com/example/parallel/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/parallel/MyFirebaseMessagingService.java
@@ -1,11 +1,31 @@
 package com.example.parallel;
 
 import android.content.SharedPreferences;
+import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+
+    public String token;
+    /**
+     * Called if FCM registration token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the
+     * FCM registration token is initially generated so this is where you would retrieve
+     * the token.
+     */
+    @Override
+    public void onNewToken(String token) {
+        Log.d("TOKEN", "Refreshed token: " + token);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // FCM registration token to your app server.
+        //sendRegistrationToServer(token);
+        this.token = token;
+    }
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
--- app/src/main/java/com/example/parallel/User.java
+++ app/src/main/java/com/example/parallel/User.java
@@ -1,8 +1,33 @@
 package com.example.parallel;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.installations.FirebaseInstallations;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.io.IOException;
+import java.util.concurrent.Executor;
+
+import javax.security.auth.callback.CallbackHandler;
+
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class User {
 
     private String fullName, email, licenseNumber;
+    public String token;
 
     public User() {
     }
@@ -40,4 +65,65 @@ public class User {
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
+
+    private void setToken(String token) {this.token = token;};
+
+    private  void sendNotification(String title, String body){
+
+        this.token = FirebaseMessaging.getInstance().getToken().getResult();
+        Log.d("TOKEN", token);
+//                .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                    public void onSuccess(TResult result){
+//
+//                    }
+//                });
+//                    @Override
+//                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+//                        if (task.isSuccessful()) {
+//                            String token = task.getResult().getToken();
+//                            setToken(token);
+//                            Log.d("TOKEN", "Refreshed token: " + token);
+//                        } else {
+//                            Log.d("TOKEN", "FAILED TO GET TOKEN");
+//                        }
+//                    }
+//                });
+//
+//        FirebaseInstallations.getToken()
+//                    .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+//                        @Override
+//                        public void onComplete(@NonNull Task<InstanceIdResult> task) {
+//                            if (task.isSuccessful()) {
+//                                String token = task.getResult().getToken();
+//                                setToken(token);
+//                                Log.d("TOKEN", "Refreshed token: " + token);
+//                            } else {
+//                                Log.d("TOKEN", "FAILED TO GET TOKEN");
+//                            }
+//                        }
+//                    });
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://androidnotificationtutorial.firebaseapp.com/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        Api api = retrofit.create(Api.class);
+
+        Call<ResponseBody> call = api.sendNotification(this.token, title, body);
+
+//        call.enqueue(new CallbackHandler<ResponseBody>() {
+//            @Override
+//            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) throws IOException {
+//                //Toast.makeText(SendNotificationActivity.this, response.body().string(), Toast.LENGTH_LONG).show();
+//            }
+//
+//            @Override
+//            public void onFailure(Call<ResponseBody> call, Throwable t) {
+//
+//            }
+//        });
+
+    }
+
 }
