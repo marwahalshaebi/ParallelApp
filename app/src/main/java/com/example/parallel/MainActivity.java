85141790953260dcaf82f2c2bbd3e4dd8b067865
t-emala <t-emala@microsoft.com>
2020-12-02 16:36:28 -0500

    --

--- app/src/main/java/com/example/parallel/MainActivity.java
+++ app/src/main/java/com/example/parallel/MainActivity.java
@@ -1,30 +1,20 @@
 package com.example.parallel;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private TextView nameText;
     private String emailExtra;
     private ImageView mProfile;
+    public static final String CHANNEL_ID = "Parallel App";
+    private static final String CHANNEL_NAME = "Main Activity";
+    private static final String CHANNEL_DESC = "Parallel App Notifications";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
