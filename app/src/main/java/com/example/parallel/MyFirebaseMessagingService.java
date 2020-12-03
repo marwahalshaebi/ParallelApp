744ea4d7d90a0bcbf7fc3de476be3b68616d2b14
Owner <hersi.abduljabbar@gmail.com>
2020-12-03 18:48:59 -0500

    Fix notification and payment

---
+++ app/src/main/java/com/example/parallel/MyFirebaseMessagingService.java
@@ -0,0 +1,60 @@
+package com.example.parallel;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import androidx.core.app.NotificationCompat;
+
+
+public class
+MyFirebaseMessagingService extends FirebaseMessagingService {
+
+
+    public String token;
+    private FirebaseUser user;
+    private String userID;
+    private DatabaseReference ref;
+    /**
+     * Called if FCM registration token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the
+     * FCM registration token is initially generated so this is where you would retrieve
+     * the token.
+     */
+    @Override
+    public void onNewToken(final String token) {
+        Log.d("TOKEN", "Refreshed token: " + token);
+
+    }
+
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.d("onMessageReceived", "Reached OnMessageReceive: ");
+        super.onMessageReceived(remoteMessage);
+        if (remoteMessage.getNotification() != null) {
+            String title = remoteMessage.getNotification().getTitle();
+            String body = remoteMessage.getNotification().getBody();
+            //NotificationHelper.displayNotification(getApplicationContext(), title, body);
+        }
+        String channelId = "Default";
+        NotificationCompat.Builder builder = new  NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.drawable.ic_baseline_directions_car_24)
+                .setContentTitle(remoteMessage.getNotification().getTitle())
+                .setContentText(remoteMessage.getNotification().getBody());
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId, "Default channel", NotificationManager.IMPORTANCE_DEFAULT);
+            manager.createNotificationChannel(channel);
+        }
+        manager.notify(0, builder.build());
+        Log.d("NOTIFICATION", "SENT NOTIFICATION ");
+    }
+
+}
--- app/src/main/java/com/example/parallel/User.java
+++ app/src/main/java/com/example/parallel/User.java
@@ -1,5 +1,20 @@
 package com.example.parallel;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class User {
 
     private String fullName, email, licenseNumber;
@@ -23,6 +38,7 @@ public class User {
         return licenseNumber;
     }
 
+
     public void setLicenseNumber(String licenseNumber) {
         this.licenseNumber = licenseNumber;
     }
@@ -36,4 +52,51 @@ public class User {
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
+
+
+    public  void sendNotification(final String title, final String body){
+
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w("FETCHING TOKEN", "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        Retrofit retrofit = new Retrofit.Builder()
+                                .baseUrl("https://parallel3004.firebaseapp.com/api/")
+                                .addConverterFactory(GsonConverterFactory.create())
+                                .build();
+
+                        Api api = retrofit.create(Api.class);
+
+                        retrofit2.Call<ResponseBody> call = api.sendNotification(token, title, body);
+
+                        call.enqueue(new Callback<ResponseBody>() { @Override
+                        public void onResponse(Call<ResponseBody> call, retrofit2.Response<ResponseBody> response) {
+                            System.out.println("========SUCCESS=========");
+                        }
+
+                            @Override
+                            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                                System.out.println("========FAILURE=========");
+                            }
+                        });
+
+
+                    }
+                });
+
+
+    }
+
+
+
+
 }
